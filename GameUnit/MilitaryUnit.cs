989e88f408cb7cc4f99f02b436224f5fe82fb11a
Syndher <miguelfilipe.dev@gmail.com>
2025-04-10 19:09:42 +0100

    Add Cost and Health overrides

--- GameUnit/MilitaryUnit.cs
+++ GameUnit/MilitaryUnit.cs
@@ -5,6 +5,10 @@ namespace GameUnit
         public int AttackPower { get; }
         public int XP { get; private set; }
 
+        public override int Health {get => base.Health + XP;}
+
+        public override float Cost {get {return AttackPower + XP;}}
+
         public MilitaryUnit(int mov, int health, int attackPower) : base(mov, health)
         {                                                        // Unit(int, int)
             AttackPower = attackPower;
